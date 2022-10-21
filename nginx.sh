04963c4b54cd3b7176c44ea31abc5bde500b3a6d
maheshmsec <maheshmeena@microsec.ai>
2022-10-21 17:07:57 +0530

    Fix

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo cp -rf app.conf /etc/nginx/sites-available/app
+sudo cp -rf app.conf /etc/nginx/sites-available/myportfolio
 chmod 710 /var/lib/jenkins/workspace/django-cicd
 
-sudo ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/myportfolio /etc/nginx/sites-enabled
 sudo nginx -t
 
 sudo systemctl start nginx
